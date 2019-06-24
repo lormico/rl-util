4ae9ed907fb7d9bbc7efdb8ea13e3eb31086cf42
Lorenzo <lor.mico@gmail.com>
2019-06-24 21:43:17 +0200

    Progressi su Parser

--- app/src/main/java/it/lmico/myapplication/Parser.java
+++ app/src/main/java/it/lmico/myapplication/Parser.java
@@ -13,63 +13,58 @@ import java.util.regex.Pattern;
 import static it.lmico.myapplication.Constants.NORTHBOUND;
 import static it.lmico.myapplication.Constants.SOUTHBOUND;
 
-public class Parser {
+class Parser {
 
-    public static Map<String, List<LocalTime>> parseChanges(String s) {
+    private static int countRegolare = 0;
+    private static int countCifre = 0;
+    private static int countPsp = 0;
+
+    private static Pattern pRegolare = Pattern.compile("regolare", Pattern.CASE_INSENSITIVE);
+    private static Pattern pCifre = Pattern.compile("[0-9]+");
+    private static Pattern pColomboGroup = Pattern.compile("(.*)([c].*)", Pattern.CASE_INSENSITIVE);
+    private static Pattern pPsp = Pattern.compile("[s]", Pattern.CASE_INSENSITIVE);
+    private static Pattern pTime = Pattern.compile("[0-9]([0-9])?[:\\.][0-9]{2}");
+
+    private static Matcher mRegolare;
+    private static Matcher mCifre;
+    private static Matcher mColomboGroup;
+    private static Matcher mPsp;
+    private static Matcher mTime;
+
+    static Map<String, List<LocalTime>> parseChanges(String s) {
 
         Map<String, List<LocalTime>> result = new HashMap<>();
         result.put(NORTHBOUND, new ArrayList<LocalTime>());
         result.put(SOUTHBOUND, new ArrayList<LocalTime>());
 
-        Pattern pRegolare = Pattern.compile("regolare", Pattern.CASE_INSENSITIVE);
-        Pattern pCifre = Pattern.compile("[0-9]+");
-        Pattern pColomboGroup = Pattern.compile("(.*)[c][a-z]+(.*)", Pattern.CASE_INSENSITIVE);
-        Pattern pPsp = Pattern.compile("[s]}", Pattern.CASE_INSENSITIVE);
-
-        Matcher mRegolare;
-        Matcher mCifre;
-        Matcher mColomboGroup;
-        Matcher mPsp;
-
-        int countRegolare = 0;
-        int countCifre = 0;
-        int countPsp = 0;
-
         try {
-
 
             // Cerca di dividere PSP e Colombo, non sapendo se venga prima uno o l'altro
             Boolean pspThenColombo = null;
             mColomboGroup = pColomboGroup.matcher(s);
             List<String> groups = new ArrayList<>();
-            while (mColomboGroup.find()) { groups.add(mColomboGroup.group()); }
+            if (mColomboGroup.matches()) {
+                groups.add(mColomboGroup.group(1));
+                groups.add(mColomboGroup.group(2));
+            }
 
             for (int i = 0; i < groups.size(); i++) {
 
+                countPsp = 0;
                 mPsp = pPsp.matcher(groups.get(i));
                 while (mPsp.find()) {
                     countPsp++;
                 }
 
                 if (countPsp != 0) {
-                    if (i == 0) {
-                        pspThenColombo = true;
-                    } else {
-                        pspThenColombo = false;
-                    }
+                    pspThenColombo = i == 0;
                 }
 
             }
 
             String groupPsp = "";
             String groupColombo = "";
-            if (pspThenColombo == true) {
-                groupPsp = groups.get(0);
-                groupColombo = groups.get(1);
-            } else if (pspThenColombo == false) {
-                groupPsp = groups.get(1);
-                groupColombo = groups.get(0);
-            } else if (pspThenColombo == null) {
+            if (pspThenColombo == null) {
                 // non sono nominati colombo o psp, mi aspetto regolare
                 mRegolare = pRegolare.matcher(s);
                 while (mRegolare.find()) { countRegolare++; }
@@ -78,38 +73,17 @@ public class Parser {
                 } else {
                     // Non gestito
                 }
+            } else if (pspThenColombo == true) {
+                groupPsp = groups.get(0);
+                groupColombo = groups.get(1);
+            } else if (pspThenColombo == false) {
+                groupPsp = groups.get(1);
+                groupColombo = groups.get(0);
             }
 
             for (String group : Arrays.asList(groupPsp, groupColombo)) {
 
-                mRegolare = pRegolare.matcher(group);
-                while (mRegolare.find()) { countRegolare++; }
 
-                mCifre = pCifre.matcher(group);
-                while (mCifre.find()) { countCifre++; }
-
-                if (countRegolare == 1) {
-
-                    if (countCifre == 0) {
-                        return result;
-                    } else {
-                        // Regolare in parte
-
-
-
-
-                        countRegolare = 0;
-                        countCifre = 0;
-
-                        mCifre = pCifre.matcher(tempRes);
-                        while (mCifre.find()) { countCifre++; }
-
-                        if (countCifre != 0) {
-
-                        }
-                    }
-
-                }
 
             }
 
@@ -122,4 +96,34 @@ public class Parser {
         return result;
     }
 
+    private List<LocalTime> getLocalTimesFromString(String s) throws Exception {
+
+        List<LocalTime> outList = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+        countRegolare = 0;
+        countCifre = 0;
+
+        mRegolare = pRegolare.matcher(s);
+        while (mRegolare.find()) { countRegolare++; }
+
+        mCifre = pCifre.matcher(s);
+        while (mCifre.find()) { countCifre++; }
+
+        if (countRegolare == 1) {
+            if (countCifre != 0) {
+                throw new Exception("Il gruppo contiene 'REGOLARE' e contemporaneamente delle cifre!");
+            }
+        } else if (countRegolare == 0){
+            mTime = pTime.matcher(s);
+            while (mTime.find()) {
+                char[] fill = new char[5-mTime.group().length()];
+                Arrays.fill(fill, '0');
+                temp.append(fill).append(mTime.group());
+            }
+        } else {
+            throw new Exception("Più di un'occorrenza di 'REGOLARE' nel gruppo!");
+        }
+
+        return outList;
+    }
 }
--- app/src/test/java/it/lmico/myapplication/ParsingUnitTest.java
+++ app/src/test/java/it/lmico/myapplication/ParsingUnitTest.java
@@ -3,11 +3,14 @@ package it.lmico.myapplication;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import static it.lmico.myapplication.Constants.NORTHBOUND;
+import static it.lmico.myapplication.Constants.SOUTHBOUND;
 import static org.junit.Assert.*;
 
 /**
@@ -18,8 +21,8 @@ import static org.junit.Assert.*;
 public class ParsingUnitTest {
 
     String partenzeRaw;
-    ArrayList<Date> expectedPsp;
-    ArrayList<Date> expectedColombo;
+    ArrayList<LocalTime> expectedPsp;
+    ArrayList<LocalTime> expectedColombo;
 
     @Before
     public void setUp() {
@@ -40,16 +43,25 @@ public class ParsingUnitTest {
     public void psp_ok() {
 
         partenzeRaw = "    Partenze da Porta San Paolo ore regolare da Colombo ore: 8.15-8.25-8.35-8.45-9.05  ";
-        expectedColombo.add(new Date());
+        expectedColombo.add(LocalTime.now());
+
+        test_expectations();
+    }
+
+    @Test
+    public void simplePspOk() {
+
+        partenzeRaw = "    Partenze da Porta San Paolo ore regolare per Colombo ore: 20:00  ";
+        expectedColombo.add(LocalTime.now());
 
         test_expectations();
     }
 
     public void test_expectations() {
 
-        Map<String, ArrayList<Date>> result = Parser.parseChanges(partenzeRaw);
-        List<Date> colombo = result.get("Colombo");
-        List<Date> psp = result.get("PSP");
+        Map<String, List<LocalTime>> result = Parser.parseChanges(partenzeRaw);
+        List<LocalTime> colombo = result.get(NORTHBOUND);
+        List<LocalTime> psp = result.get(SOUTHBOUND);
         assertEquals(colombo, expectedColombo);
         assertEquals(psp, expectedPsp);
     }
